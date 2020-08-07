e4084f2c1fe6d5fc2b7fa9d1a5051e86b8dd6dce
JumpAlang <1040927107@qq.com>
2020-08-08 03:32:15 +0800

    1.可查看房间用户 2.点歌列表新增随机模式

--- src/config/environment.js
+++ src/config/environment.js
@@ -9,9 +9,9 @@ let baseUrl = 'http://127.0.0.1';
 if (process.env.NODE_ENV === "development") {
     baseUrl = "http://127.0.0.1:8080";
 } else if (process.env.NODE_ENV === "test") {
-    baseUrl = "http://120.24.243.237:8080";
+    baseUrl = "http://www.alang.run:8080";
 } else if (process.env.NODE_ENV === "production") {
-    baseUrl = "http://120.24.243.237:8080";
+    baseUrl = "http://www.alang.run:8080";
 }
 
 let isProduction = false;
